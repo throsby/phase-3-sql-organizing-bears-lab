d5bff3d184800a918008815126ed73ffa01b92f3
Throsby Wells <throsbywells@gmail.com>
2022-04-29 13:50:13 -0400

    commit tree 82db7c53b0e3e6bd55da10c6a527bdb9c1ecbd11

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES  ("Mr. Chocolate", 12, "M","purple","agro",true),
+        ("Rowdy", 12, "M","purple","agro",true),
+        ("Tabitha", 12, "F","purple","agro",true),
+        ("Sargeant Brown", 12, "F","purple","agro",true),
+        ("Melissa", 12, "M","purple","agro",true),
+        ("Grinch", 12, "F","green","sad",true),
+        ("Wendy", 12, "M","purple","agro",true),
+        (NULL, 12, "M","purple","agro",true);
